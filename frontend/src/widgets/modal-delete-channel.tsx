26678f6dfd1a9ccc17733d66b47513932bb680c4
Атаманов Дмитрий <atamanov.job@gmail.com>
2026-05-31 02:21:22 +0500

    Работа с каналами

--- frontend/src/widgets/modal-delete-channel.tsx
+++ frontend/src/widgets/modal-delete-channel.tsx
@@ -1,16 +1,40 @@
 import { Modal, Button } from "react-bootstrap";
-import { useDeleteChannelMutation } from "../features/model/channels/channels.api";
+import {
+  useDeleteChannelMutation,
+  useLazyFetchChannelsQuery,
+} from "../features/model/channels/channels.api";
 import { channelsActions } from "../features/model/channels/channels.slice";
 import { useAppSelector, useAppDispatch } from "../@redux/hooks";
 import { toast } from "react-toastify";
+import {
+  useDeleteMessageMutation,
+  useFetchMessagesQuery,
+} from "../features/model/messages/messages.api";
 
 const ModalDeleteChannel = () => {
-  const [deleteChannel] = useDeleteChannelMutation();
   const dispatch = useAppDispatch();
+
+  const [fetchChannels] = useLazyFetchChannelsQuery();
+  const [deleteChannel] = useDeleteChannelMutation();
+  const { data: messages } = useFetchMessagesQuery();
+  const [deleteMessage] = useDeleteMessageMutation();
 
   const { id, isOpen } = useAppSelector(
     (state) => state.channels.modalDeleteOpen,
   );
+
+  const { selectedChannel } = useAppSelector((state) => state.channels);
+
+  const deleteMessages = () => {
+    if (messages === undefined) {
+      return [];
+    }
+
+    return messages
+      .filter(({ channelId }) => channelId === id)
+      .map(({ id }) => deleteMessage({ id }).unwrap());
+  };
+
   const closeModal = () => {
     dispatch(channelsActions.setIsModalDeleteOpen({ isOpen: false, id: null }));
   };
@@ -22,10 +46,19 @@ const ModalDeleteChannel = () => {
     }
 
     try {
-      await deleteChannel({ id }).unwrap();
+      await Promise.all([deleteChannel({ id }).unwrap(), ...deleteMessages()]);
 
       closeModal();
       toast.success("Канал удалён");
+
+      if (id !== selectedChannel?.id) {
+        return;
+      }
+
+      const channels = await fetchChannels().unwrap();
+      if (channels[0]) {
+        dispatch(channelsActions.select(channels[0]));
+      }
     } catch (error) {
       console.log(error);
       toast.error("Ошибка сети");
